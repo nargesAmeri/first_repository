7855a32ae2c7311cee01b26ee60e1e37115b818d
Narjes <128999603+nargesAmeri@users.noreply.github.com>
2023-09-10 22:14:50 +0330

    Add files via upload

---
+++ days.ts
@@ -0,0 +1,26 @@
+let d : number= 5;
+if (d ==1){
+    
+    console.log("saturday");
+}
+if (d==2){
+    console.log("sunday");
+}
+if (d==3){
+    console.log("monday");
+}
+if (d ==4){
+    
+    console.log("tuesday");
+    
+}
+if (d==5){
+    console.log("wednesday");
+}
+if (d==6){
+    console.log("thursday");
+}
+if (d==7){
+    console.log("friday");
+}
+
