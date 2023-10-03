501daf5df6776aa38da910f19bccc0de72b8b9dd
Narjes <128999603+nargesAmeri@users.noreply.github.com>
2023-10-03 10:10:57 +0330

    Add files via upload

---
+++ purchase_basket.ts
@@ -0,0 +1,6 @@
+let basket=[{ name:"milk", price:26000}, { name:"strawberry", price:56000},{ name:"cheese", price:34000},{ name:"chips", price:15000}];
+if (!basket[0]){
+    console.warn("sabad khali ast...")
+}
+else{
+console.log(basket);}
